881cf183551ecf68dd4189488f4c33e05b18af8e
r-svp26 <riteshk6341@gmail.com>
2021-08-26 23:07:47 +0530

    [Ritesh] Add: Insert values into employee_payroll table

--- EmployeePayroll/Employee.sql
+++ EmployeePayroll/Employee.sql
@@ -15,3 +15,12 @@ Name varchar(255),
 Salary int,
 StartDate date
 )
+
+/* Insert into employee_payroll */
+INSERT INTO employee_payroll(Name, Salary, StartDate ) VALUES ('Ritesh', 50000, '2010-01-01')
+INSERT INTO employee_payroll(Name, Salary, StartDate ) VALUES ('Bill', 40000, '2018-01-01')
+INSERT INTO employee_payroll(Name, Salary, StartDate ) VALUES ('Charlie', 30000, '2016-04-07')
+
+
+
+
