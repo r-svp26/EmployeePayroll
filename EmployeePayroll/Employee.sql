6c82234cf9fc12fd0cf03b0c75924a848eef318b
r-svp26 <riteshk6341@gmail.com>
2021-08-26 22:59:03 +0530

    [Ritesh] Add: Create employee_payroll table

--- EmployeePayroll/Employee.sql
+++ EmployeePayroll/Employee.sql
@@ -1,2 +1,17 @@
 /* Create Database */
 CREATE DATABASE payroll_service
+
+/* Show the Databases */
+SELECT * 
+FROM Sys.Databases
+
+/* Use Database */
+USE payroll_service
+
+/* Create Table */
+CREATE TABLE employee_payroll(
+ID int identity (1,1) primary key,
+Name varchar(255),
+Salary int,
+StartDate date
+)
